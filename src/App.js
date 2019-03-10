91d351e2892e3a44065f81c2590731eb4d473847
Nguyet97 <nhunguyet201297@gmail.com>
2019-03-11 01:46:03 +0700

    cm

---
+++ src/App.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as contactAction from './actions/contactAction';
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+     
+    this.state = {
+      name: ''
+    }
+  }
+
+  handleChange(e){
+    this.setState({
+      name: e.target.value
+    })
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    let contact = {
+      name: this.state.name
+    }
+    this.setState({
+      name: ''
+    });
+    this.props.createContact(contact);
+  }
+
+  listView(data, index){
+    return (
+      <div className="row">
+        <div className="col-md-10">
+          <li key={index} className="list-group-item clearfix">
+            {data.name}
+          </li>
+        </div>
+        <div className="col-md-2">
+          {/* <button onClick={(e) => this.editcontact(e, index)} className="btn btn-danger" style={buttonStyle}>
+            Edit
+          </button> */}
+          <button onClick={(e) => this.deleteContact(e, index)} className="btn btn-danger">
+            Remove
+          </button>
+          
+        </div>
+    </div> 
+    )
+  }
+
+  deleteContact(e, index){
+    e.preventDefault();
+    this.props.deleteContact(index);
+  }
+  render() {
+
+    return(
+      <div className="container" style={divStyle}>
+        <h1 style={h1Style}>DANH SÁCH MÓN ĂN</h1>
+        <hr />
+        <div>
+          <h3>Thêm món</h3>
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" onChange={this.handleChange} className="form-control" value={this.state.name}/><br />
+            <input type="submit" className="btn btn-success" value="ADD"/>
+          </form>
+          <hr />
+        { <ul className="list-group">
+          {this.props.contacts.map((contact, i) => this.listView(contact, i))}
+        </ul> }
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    contacts: state.contacts
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    createContact: contact => dispatch(contactAction.createContact(contact)),
+    deleteContact: index =>dispatch(contactAction.deleteContact(index))
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+////style
+const divStyle={
+  marginTop: '40px'
+};
+const h1Style={
+  textAlign: 'center',
+  color: 'grey',
+  fontStyle: 'italic'
+
+};
+
